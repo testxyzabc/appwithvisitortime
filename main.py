908a5aa6b65efc7448f35690d344ac341d6347a4
Himanshu <rathihimanshu@live.com>
2023-04-24 22:53:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Copyright 2018 Googl
-# [START gae_python38_render_template]
 import datetime
 from google.cloud import datastore
 from flask import Flask, render_template
@@ -41,13 +39,3 @@ def root():
     return render_template('index.html', times=times)
 
 
-#if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    #app.run(host='127.0.0.1', port=8080, debug=True)
-# [START gae_python38_render_template]
